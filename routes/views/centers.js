1a4de3b100abab1e30225afbe35616a07452db8c
yashaswi maharshi <maha.yash@gmail.com>
2018-05-31 18:30:20 +0530

    display the center name from mongo

--- routes/views/centers.js
+++ routes/views/centers.js
@@ -3,13 +3,14 @@ var keystone = require("keystone");
 exports = module.exports = function(req, res) {
   var view = new keystone.View(req, res);
   var locals = res.locals;
+  locals.data = {
+    centers: []
+  };
 
-  // locals.section is used to set the currently selected
-  // item in the header navigation.
   locals.section = "centers";
   var centerInfo = keystone.list("Center").model.find();
   centerInfo.exec(function(err, result) {
-	  console.log('result '+ result);
+    locals.data.centers = result;
   });
   view.render("centers");
 };
