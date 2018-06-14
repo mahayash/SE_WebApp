52e3d49a5a35bd3aacc178bdde902ee865fb2118
yashaswi maharshi <maha.yash@gmail.com>
2018-06-14 06:37:53 +0530

    UI - Colors

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,16 @@ $(document).ready(function() {
   $("#splotLight").slick({
     dots: true,
     autoplay: false,
-    infinite : true,
-    arrows :false
+    infinite: true,
+    arrows: false
+  });
+
+  $(".pillarTeam").slick({
+    dots: true,
+    autoplay: false,
+    infinite: true,
+    arrows: true,
+    centerMode: true,
+    slideToShow : 4
   });
 });
