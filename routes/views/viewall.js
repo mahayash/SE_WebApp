9c1376703f6aa929cca55a7f19f45a68dcb0d004
yashaswi maharshi <maha.yash@gmail.com>
2018-06-17 12:18:03 +0530

    done with the view all page

--- routes/views/viewall.js
+++ routes/views/viewall.js
@@ -3,10 +3,21 @@ exports = module.exports = function(req, res) {
   let view = new keystone.View(req, res);
   let locals = res.locals;
   locals.data = {
-    studentInCategory: []
+    studentInCategory: [],
+    category: []
   };
 
-  //console.log(req.params.category);
+  switch (parseInt(req.params.category)) {
+    case 1:
+      locals.data.category = "The Toppers";
+      break;
+    case 2:
+      locals.data.category = "The Highest";
+      break;
+    case 3:
+      locals.data.category = "The Achievers";
+      break;
+  }
 
   let studentInCategoryQuery = keystone
     .list("StudentScore")
@@ -16,10 +27,11 @@ exports = module.exports = function(req, res) {
 
   studentInCategoryQuery.exec(function(err, result) {
     if (result != null) {
-      console.log("result " + result);
       locals.data.studentInCategory = result;
     } else {
-      console.log("Error " + err);
+      return res
+        .status(404)
+        .send(keystone.wrapHTMLError("Sorry, no records found"));
     }
   });
 
